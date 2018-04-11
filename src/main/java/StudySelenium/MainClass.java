592e4cb5a50533c92e61320522da4228452c5eab
Roman Belov <r.belov@itwebnet.ru>
2018-04-11 21:08:46 +0300

    Первый коммит учебного проекта

---
+++ src/main/java/StudySelenium/MainClass.java
@@ -0,0 +1,13 @@
+package StudySelenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MainClass {
+    public static void main(String[] args) {
+           System.setProperty("webdriver.chrome.driver","D:\\Java\\StudySelenium\\drivers\\chromedriver.exe");
+           WebDriver driver = new ChromeDriver();
+           driver.get("https://www.google.ru/");
+    }
+
+}
